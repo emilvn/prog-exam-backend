566fe79bbf0a02c137cbaf6588a4e6a4ed4701b5
emilvn <emilvnielsen@hotmail.com>
2024-06-19 20:53:42 +0200

    fixed hibernate not creating tables

--- src/main/java/dk/emilvn/exam/config/InitData.java
+++ src/main/java/dk/emilvn/exam/config/InitData.java
@@ -1,9 +1,27 @@
 package dk.emilvn.exam.config;
 
+import dk.emilvn.exam.example.ExampleDTO;
+import dk.emilvn.exam.example.ExampleService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("!test")
-public class InitData {
+public class InitData implements CommandLineRunner {
+
+    private final ExampleService exampleService;
+
+    public InitData(ExampleService exampleService) {
+        this.exampleService = exampleService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        createExampleData();
+    }
+
+    private void createExampleData() {
+        exampleService.create(new ExampleDTO(null, "Example 1"));
+    }
 }
